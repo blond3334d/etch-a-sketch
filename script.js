d93ed830ae1ce391cfd4c63a91add520e4d2529e
Adrian <264207672+blond3334d@users.noreply.github.com>
2026-06-27 22:41:04 +0800

    Structure the layout

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const boxContainer = document.querySelector("#container");
+const boxContainer = document.querySelector(".container");
+let userInput = 16;
 
-const grid = document.createElement("div");
+for (let row = 0; row < userInput; row++) {
+    const grid = document.createElement('div');
+    grid.setAttribute('class', 'row');
+    for (let squares = 0; squares < userInput; squares++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        grid.appendChild(square);
+    }
 
-boxContainer.appendChild(grid);
+        boxContainer.appendChild(grid);
+}
